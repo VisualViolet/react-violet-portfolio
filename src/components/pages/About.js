d6247feb73dbc088e9a1fc4060d7bee5db35eda2
VisualViolet <savannah_b_miller@outlook.com>
2023-03-02 17:57:33 -0800

    added content to About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,12 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Hi, I'm Savannah.</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      I am a student enrolled in the UW Coding Bootcamp. This bootcamp teaches in-demand skills such as HTML5, CSS, JavaScript, jQuery, and the MERN stack. I have previous coding experience with HTML, CSS, SQL, and C# and am seeking an opportunity to use this combination of new and learned skills in a technical environment.
+      </p>
+      <p>
+      Over the course of my schooling, I have contributed to and completed several projects. I strive to build attractive, responsive, and accessibility compliant websites while still showing off my creativity. Learning how to code is learning how to create!
       </p>
     </div>
   );
