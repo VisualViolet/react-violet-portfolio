29e42719d58805d9caaa77bfa8fd0bf5b1732beb
VisualViolet <savannah_b_miller@outlook.com>
2023-03-02 19:13:58 -0800

    added footer content and material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Resume from "./components/pages/Resume";
 import Header from "./Header";
+import Footer from "./Footer";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const App = () => {
   return (
@@ -18,6 +24,7 @@ const App = () => {
           <Route path="/contact" element = {<Contact/>}/>
           <Route path="/resume" element = {<Resume/>}/>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <div>
+        <ul>
+            <li>Github</li>
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+        </ul>
+      </div>
+    )
+  };
+
+  export default Footer;
